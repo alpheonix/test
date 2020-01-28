9275e5aa75757784db5d43bce8d2c672e4aad593
arthur <arthurblanc98@gmail.com>
2020-01-28 14:17:20 +0100

    jazzy

--- Test2020/Algo2020.swift
+++ Test2020/Algo2020.swift
@@ -17,7 +17,19 @@ class Algo2020 {
     }
     
     
-    /// isLess: This is a function
+     /**
+        Call this function for showing alert with OK and Cancel button in your View Controller class.
+        - Parameters:
+           - VC : View Controller over which the function is called. You can use self, or provide view controller name.
+           - message: Pass your alert message in String.
+           - okClickHandler: This will give you call back inside block when OK button is clicked
+        
+        ### Usage Example: ###
+        ````
+        AlertClass().showAlert(self, andMessage: "This is custom alert") { (okClick) in
+        }
+        ````
+        */
     func isLess(than val: Int) -> Bool {
         return val < self.val
     }
