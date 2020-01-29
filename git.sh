d3c3264da4c30224ef053ca19a3a32c9755fd0a4
arthur <arthurblanc98@gmail.com>
2020-01-29 13:49:36 +0100

    branch

--- git.sh
+++ git.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/alpheonix/test.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/alpheonix/test.git > /dev/null 2>&1
   git push origin tests:master
 }
 
