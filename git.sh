307e88151be7e8d465c044b879b357b9c60d4886
arthur <arthurblanc98@gmail.com>
2020-01-29 10:48:02 +0100

    branch

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git branch
-git remote set-url origin git@github.com/alpheonix/test.git
+git remote set-url https://github.com/alpheonix/test.git
 git config --global user.name "alpheonix"
 git config --global user.email arthurblanc98@gmail.com
 git add fastlane/xcov_report/index.html
