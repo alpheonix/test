6275fd1121547de2a0480bd1a4de9eb55436b7be
arthur <arthurblanc98@gmail.com>
2020-01-29 10:00:38 +0100

    branch

--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git branch
+git config --global user.name "alpheonix"
+git config --global user.email arthurblanc98@gmail.com
 git add fastlane/xcov_report/index.html
 git commit -m"test commit travis"
 git push origin tests:master
